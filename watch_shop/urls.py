ef55520f180f1be24e573e0ac4a99b6bdd5ef040
Талгат Чекиров <talgat.chekirov@gmail.com@=>
2023-09-18 14:04:06 +0600

    second_hw_2

--- watch_shop/urls.py
+++ watch_shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wath_list/', views.watch_shop_view, name='watches'),
+    path('watch_list/', views.watch_shop_view, name='watches'),
     path('watch_detail/<int:id>', views.watch_shop_detail_view, name='watch_detail'),
 ]
