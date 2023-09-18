c630d8225983f5756fae90d54fd60664b593d880
Талгат Чекиров <talgat.chekirov@gmail.com@=>
2023-09-18 13:54:27 +0600

    second_hw

--- main/settings.py
+++ main/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library',
+    'watch_shop',
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path('', include('watch_shop.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
                +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
